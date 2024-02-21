724924dd3e7cb54fdb161b69bee8097ec2aeca7f
abhithakur2 <156669712+abhithakur2@users.noreply.github.com>
2024-02-21 17:36:44 +0530

    Update jobBuilder.groovy

--- vars/jobBuilder.groovy
+++ vars/jobBuilder.groovy
@@ -111,7 +111,7 @@ spec:
                                     url("${entry.getKey()}")
                                     credentials('git_read')
                                 } 
-                                branch ('\${BRANCH}')
+                                branch ('\PMIDC-Upgrade-Dev')
                                 scriptPath('Jenkinsfile')
                                 extensions { }
                             }
