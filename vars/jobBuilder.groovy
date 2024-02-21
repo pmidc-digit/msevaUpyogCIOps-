1f80b8816d73a102dfc71d243c4dc1432262ed0d
abhithakur2 <156669712+abhithakur2@users.noreply.github.com>
2024-02-21 17:23:38 +0530

    Update jobBuilder.groovy
    update the new branch 'PMIDC-Upgrade-Dev'

--- vars/jobBuilder.groovy
+++ vars/jobBuilder.groovy
@@ -93,7 +93,7 @@ spec:
                         description('') 
                         branch('')      
                         useRepository('')                     
-                        defaultValue('origin/master') 
+                        defaultValue('origin/PMIDC-Upgrade-Dev') 
                         branchFilter('.*')
                         tagFilter('*')
                         sortMode('ASCENDING_SMART')
